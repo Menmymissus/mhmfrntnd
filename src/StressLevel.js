3f72d684135d6a345fa33c9b8cf9c4c6ff899792
Menmymissus <legendkiller11918114@gmail.com>
2023-05-12 15:45:30 +0545

    mhmfntend

--- src/StressLevel.js
+++ src/StressLevel.js
@@ -1,6 +1,29 @@
 import React from "react";
 import axios from "axios";
+import './static/css/StressLevel.css';
 import { useState } from "react";
+import img1 from './static/images/StressImages/anxietylevel.png';
+import img2 from './static/images/StressImages/selfesteem.jpg';
+import img3 from './static/images/StressImages/mentalhealth.avif';
+import img4 from './static/images/StressImages/depression1.jpg';
+import img5 from './static/images/StressImages/headache.webp';
+import img6 from './static/images/StressImages/bloodpressure.webp';
+import img7 from './static/images/StressImages/sleepquality.jpg';
+import img8 from './static/images/StressImages/breathingproblem.avif';
+import img9 from './static/images/StressImages/noise.jpg';
+import img10 from './static/images/StressImages/livingcondition.jpg';
+import img11 from './static/images/StressImages/safety.avif';
+import img12 from './static/images/StressImages/basicneed.jpg';
+import img13 from './static/images/StressImages/academicperformance.avif';
+import img14 from './static/images/StressImages/studyload.avif';
+import img15 from './static/images/StressImages/teacherstudentrelationship.avif';
+import img16 from './static/images/StressImages/futurecareer.avif';
+import img17 from './static/images/StressImages/socialsupport.avif';
+import img18 from './static/images/StressImages/peerpressure.avif';
+import img19 from './static/images/StressImages/extraactivity.avif';
+import img20 from './static/images/StressImages/bullying.avif';
+
+
 
 const api_url = "/api/predict/";
 const StressPredictionForm = () => {
@@ -64,9 +87,10 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="anxiety_level" className="form-label">
-              Anxiety Level
+              
+              <img src={img1} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Anxiety level" 
               type="number"
               id="anxiety_level"
               name="anxiety_level"
@@ -80,9 +104,10 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="self_esteem" className="form-label">
-              Self Esteem
+            <img src={img2} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
+              
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Self Esteem" 
               type="number"
               id="self_esteem"
               name="self_esteem"
@@ -96,9 +121,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="mental_health_history" className="form-label">
-              Mental Health History
+            <img src={img3} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Mental Health History"
               type="number"
               id="mental_health_history"
               name="mental_health_history"
@@ -112,9 +137,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="depression" className="form-label">
-              Depression
+            <img src={img4} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Depression"
               type="number"
               id="depression"
               name="depression"
@@ -128,9 +153,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="headache" className="form-label">
-              Headache
+            <img src={img5} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Headache"
               type="number"
               id="headache"
               name="headache"
@@ -144,9 +169,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="blood_pressure" className="form-label">
-              Blood Pressure
+            <img src={img6} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Blood Pressure"
               type="number"
               id="blood_pressure"
               name="blood_pressure"
@@ -160,9 +185,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="sleep_quality" className="form-label">
-              Sleep Quality
+            <img src={img7} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Sleep quality"
               type="number"
               id="sleep_quality"
               name="sleep_quality"
@@ -176,9 +201,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="breathing_problem" className="form-label">
-              Breathing Problem
+            <img src={img8} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Breathing Problem"
               type="number"
               id="breathing_problem"
               name="breathing_problem"
@@ -192,9 +217,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="noise_level" className="form-label">
-              Noise Level
+            <img src={img9} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Noise level"
               type="number"
               id="noise_level"
               name="noise_level"
@@ -209,9 +234,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="living_conditions" className="form-label">
-              Living Conditions
+            <img src={img10} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Living Conditions"
               type="number"
               id="living_conditions"
               name="living_conditions"
@@ -225,9 +250,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="safety" className="form-label">
-              Safety
+            <img src={img11} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Safety"
               type="number"
               id="safety"
               name="safety"
@@ -241,9 +266,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="basic_needs" className="form-label">
-              Basic Needs
+            <img src={img12} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Basic Needs"
               type="number"
               id="basic_needs"
               name="basic_needs"
@@ -257,9 +282,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="academic_performance" className="form-label">
-              Academic Performance
+            <img src={img13} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Academice Performance"
               type="number"
               id="academic_performance"
               name="academic_performance"
@@ -273,9 +298,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="study_load" className="form-label">
-              Study Load
+            <img src={img14} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Study Load"
               type="number"
               id="study_load"
               name="study_load"
@@ -292,9 +317,9 @@ const StressPredictionForm = () => {
               htmlFor="teacher_student_relationship"
               className="form-label"
             >
-              Teacher-Student Relationship
+              <img src={img15} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Teacher Student Relationship"
               type="number"
               id="teacher_student_relationship"
               name="teacher_student_relationship"
@@ -308,9 +333,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="future_career_concerns" className="form-label">
-              Future Career Concerns
+            <img src={img16} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Future Career Concerns"
               type="number"
               id="future_career_concerns"
               name="future_career_concerns"
@@ -324,9 +349,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="social_support" className="form-label">
-              Social Support
+            <img src={img17} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Social Support"
               type="number"
               id="social_support"
               name="social_support"
@@ -340,9 +365,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="peer_pressure" className="form-label">
-              Peer Pressure
+            <img src={img18} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Peer Pressure"
               type="number"
               id="peer_pressure"
               name="peer_pressure"
@@ -356,9 +381,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="extracurricular_activities" className="form-label">
-              Extracurricular Activities
+            <img src={img19} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Extracurricular Activities"
               type="number"
               id="extracurricular_activities"
               name="extracurricular_activities"
@@ -372,9 +397,9 @@ const StressPredictionForm = () => {
         return (
           <div>
             <label htmlFor="bullying" className="form-label">
-              Bullying
+            <img src={img20} style={{ objectFit:"cover", width:"500px", height:"100%"}}/>
             </label>
-            <input
+            <input style={{border:"solid 2px",width:"480px",onClick:"noChange"}} placeholder="Bullying"
               type="number"
               id="bullying"
               name="bullying"
